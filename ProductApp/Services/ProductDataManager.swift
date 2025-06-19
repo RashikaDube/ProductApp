e5e9ffe7f6c2ad0cf1f45c134ed75dac4487055d
RashikaDube <99325691+RashikaDube@users.noreply.github.com>
2025-06-19 10:02:08 +0530

    Added error handling in Generic, SwiftData CRUD in separate file, image placeholders, and network management
    - Implemented error handling in Generic module
    - Moved SwiftData CRUD operations to a dedicated file for better separation of concerns
    - Added placeholder support to image list UI
    - Separated network connectivity logic into a standalone file for maintainability

---
+++ ProductApp/Services/ProductDataManager.swift
@@ -0,0 +1,18 @@
+//
+//  ProductManager.swift
+//  ProductApp
+//
+//  Created by Neosoft on 18/06/25.
+//
+
+import SwiftUI
+
+struct ProductManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductManager()
+}
